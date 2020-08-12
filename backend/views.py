e472cb1fb3fa3e06eaa2f1f29ce54f21c9cc1ba7
yinqiuliang <957919102@qq.com>
2020-08-12 17:41:36 +0800

    添加验证码view

--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 from repository import models
+from utils.utilsfile import create_validate_code
+from io import BytesIO
 # Create your views here.
 
 
 def user_register(request):
     return render(request,"user_register.html")
+
+
+def check_code(request):
+    stream = BytesIO()
+    print(stream)
+    img, code = create_validate_code()
+    print(img,code)
+    img.save(stream, 'PNG')
+
+    request.session['CheckCode'] = code
+    return HttpResponse(stream.getvalue())
