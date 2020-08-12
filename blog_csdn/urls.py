5eae601edb4d6a173da8c24ce32118053f5a9614
yinqiuliang <957919102@qq.com>
2020-08-12 16:38:07 +0800

    添加注册url入口

--- blog_csdn/urls.py
+++ blog_csdn/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from backend import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^user_register.html$', views.user_register),
 ]
