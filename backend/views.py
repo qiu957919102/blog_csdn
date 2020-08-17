ae688b10850304a1f540ede2f3b37ef7e30a6c2d
yinqiuliang <957919102@qq.com>
2020-08-17 23:47:54 +0800

    修改login为form组件验证

--- backend/views.py
+++ backend/views.py
@@ -5,8 +5,9 @@ from utils.utilsfile import create_validate_code
 from io import BytesIO
 from django.core import serializers
 # Create your views here.
-
-
+from utils.auth import check_login
+from backend.forms.account import LoginForm
+import json
 def user_register(request):
     """
     注册方法
@@ -70,42 +71,84 @@ def check_code(request):
     request.session['CheckCode'] = code
     return HttpResponse(stream.getvalue())
 
+# def user_login(request):
+#     """
+#     用户登录方法
+#     :param request:
+#     :return:
+#     """
+#     if request.method == 'GET':
+#         return render(request, "user_login.html")
+#     if request.method == 'POST':
+#         result = {'status': False, 'message': None, 'data': None}
+#         CheckCode = request.session.get("CheckCode",None)
+#         username = request.POST.get("username")
+#         password = request.POST.get("password1")
+#         code_ver = request.POST.get("code_ver")
+#         rmb = request.POST.get("rmb")
+#         # print(request.POST)
+#         if all([username, password, CheckCode]):
+#             if CheckCode.upper() == code_ver.upper():
+#                 user_info = models.UserInfo.objects.filter(username=username,password=password).only('nid', 'nickname','username', 'email','avatar',)
+#                 # user_info = models.UserInfo.objects.filter(username=username,password=password).values('nid', 'nickname','username', 'email','avatar',)
+#                 # print(user_info)
+#
+#                 if user_info:
+#                     result['status'] = True
+#                     user_info = serializers.serialize('json',user_info)
+#                     # print(user_info)
+#                     request.session['user_info'] = user_info
+#                     if rmb:
+#                         request.session.set_expiry(60 * 60 * 24 * 30)
+#                     return render(request, "index.html")
+#                 else:
+#                     result['message'] = '用户名或者密码错误'
+#             else:
+#                 result['message'] = '验证码错误'
+#         else:
+#             result['message'] = '请填写完毕登录信息'
+
 def user_login(request):
     """
-    用户登录方法
+    登陆
     :param request:
     :return:
     """
-    if request.method == 'POST':
+    if request.method == 'GET':
+        return render(request, 'user_login.html')
+    elif request.method == 'POST':
         result = {'status': False, 'message': None, 'data': None}
-        CheckCode = request.session.get("CheckCode",None)
-        username = request.POST.get("username")
-        password = request.POST.get("password1")
-        code_ver = request.POST.get("code_ver")
-        rmb = request.POST.get("rmb")
-        # print(request.POST)
-        if all([username, password, CheckCode]):
-            if CheckCode.upper() == code_ver.upper():
-                user_info = models.UserInfo.objects.filter(username=username,password=password).only('nid', 'nickname','username', 'email','avatar',)
-                # user_info = models.UserInfo.objects.filter(username=username,password=password).values('nid', 'nickname','username', 'email','avatar',)
-                print(user_info)
-
-                if user_info:
-                    result['status'] = True
-                    user_info = serializers.serialize('json',user_info)
-                    print(user_info)
-                    request.session['user_info'] = user_info
-                    if rmb:
-                        request.session.set_expiry(60 * 60 * 24 * 30)
-                    return render(request, "index.html")
-                else:
-                    result['message'] = '用户名或者密码错误'
+        form = LoginForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user_info = models.UserInfo.objects. \
+                filter(username=username, password=password). \
+                values('nid', 'nickname',
+                       'username', 'email',
+                       'avatar',
+                       'blog__nid',
+                       'blog__site').first()
+            print(user_info)
+            if not user_info:
+                # result['message'] = {'__all__': '用户名或密码错误'}
+                result['message'] = '用户名或密码错误'
             else:
-                result['message'] = '验证码错误'
+                result['status'] = True
+                request.session['user_info'] = user_info
+                if form.cleaned_data.get('rmb'):
+                    request.session.set_expiry(60 * 60 * 24 * 30)
         else:
-            result['message'] = '请填写完毕登录信息'
-    return render(request,"user_login.html")
+            print(form.errors)
+            if 'check_code' in form.errors:
+                result['message'] = '验证码错误或者过期'
+            else:
+                result['message'] = '用户名或密码错误'
+        return HttpResponse(json.dumps(result))
 
 
+
+@check_login
 def admin_index(request):
+    print(request.session.values())
     return render(request,"admin_index.html")
