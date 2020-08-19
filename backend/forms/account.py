21e9d542b0158d75e7c6150109fd86270a9ac335
yinqiuliang <957919102@qq.com>
2020-08-19 14:53:26 +0800

    form组件指定class和label

--- backend/forms/account.py
+++ backend/forms/account.py
@@ -44,25 +44,37 @@ class LoginForm(BaseForm, django_forms.Form):
 
 class RegisterForm(BaseForm, django_forms.Form):
     username = django_fields.CharField(
-        error_messages={'required': '用户名不能为空.'}
+        error_messages={'required': '用户名不能为空.'},
+        widget=django_widgets.TextInput(attrs={'class':'form-control'}),
+        label='用户名'
     )
     """
     # 定义为密码文本,render_value设置为验证不通过时不把密码刷新掉
     """
     password1 = django_fields.CharField(
-        error_messages={'required': '密码不能为空.'}
+        error_messages={'required': '密码不能为空.'},
+        widget=django_widgets.PasswordInput(attrs={'class': 'form-control'}),
+        label='密码'
     )
     password2 = django_fields.CharField(
-        error_messages={'required': '确认密码不能为空.'}
+        error_messages={'required': '确认密码不能为空.'},
+        widget=django_widgets.PasswordInput(attrs={'class': 'form-control'}),
+        label = '确认密码'
     )
     check_code = django_fields.CharField(
-        error_messages={'required': '验证码不能为空.'}
+        error_messages={'required': '验证码不能为空.'},
+        widget=django_widgets.TextInput(attrs={'class': 'form-control'}),
+        label='验证码'
     )
     nickname = django_fields.CharField(
-        error_messages={'required': '昵称不能为空.'}
+        error_messages={'required': '昵称不能为空.'},
+        widget=django_widgets.TextInput(attrs={'class': 'form-control'}),
+        label='昵称'
     )
     email = django_fields.EmailField(
-        error_messages={'required': '验证码不能为空.','invalid': '邮箱格式错误'}
+        error_messages={'required': '验证码不能为空.','invalid': '邮箱格式错误'},
+        widget=django_widgets.EmailInput(attrs={'class': 'form-control'}),
+        label='邮箱'
     )
 
     def clean(self):
