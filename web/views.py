34983f5ce9d53a34d3674e6e31a89e9764bb5935
yinqiuliang <957919102@qq.com>
2020-08-19 14:59:11 +0800

    添加测试方法

--- web/views.py
+++ web/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from utils.auth import check_login
 # Create your views here.
 
-
+@check_login
 def index(request):
+    print(request.session.get('user_info'))
     return render(request,"index.html")
